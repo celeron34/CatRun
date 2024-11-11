fcbd7e942a1c335e6737f4451ab99649ceeb2c21
Miyaichi <miyahashi.ichiru@gmail.com>
2024-11-12 02:29:20 +0900

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CatRun
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.IntervalTimer = new System.Windows.Forms.Timer(this.components);
+            this.FrameTimer = new System.Windows.Forms.Timer(this.components);
+            this.WhiteCat = new System.Windows.Forms.RadioButton();
+            this.BlackCat = new System.Windows.Forms.RadioButton();
+            this.CatIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.TaskTrayButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.TasktrayCheck = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // IntervalTimer
+            // 
+            this.IntervalTimer.Enabled = true;
+            this.IntervalTimer.Interval = 1500;
+            this.IntervalTimer.Tick += new System.EventHandler(this.IntervalTimer_Tick);
+            // 
+            // FrameTimer
+            // 
+            this.FrameTimer.Enabled = true;
+            this.FrameTimer.Tick += new System.EventHandler(this.FrameTimer_Tick);
+            // 
+            // WhiteCat
+            // 
+            this.WhiteCat.AutoSize = true;
+            this.WhiteCat.Location = new System.Drawing.Point(13, 13);
+            this.WhiteCat.Margin = new System.Windows.Forms.Padding(4);
+            this.WhiteCat.Name = "WhiteCat";
+            this.WhiteCat.Size = new System.Drawing.Size(55, 19);
+            this.WhiteCat.TabIndex = 0;
+            this.WhiteCat.Text = "白猫";
+            this.WhiteCat.UseVisualStyleBackColor = true;
+            this.WhiteCat.CheckedChanged += new System.EventHandler(this.ColorButton_CheckedChanged);
+            // 
+            // BlackCat
+            // 
+            this.BlackCat.AutoSize = true;
+            this.BlackCat.Location = new System.Drawing.Point(13, 40);
+            this.BlackCat.Margin = new System.Windows.Forms.Padding(4);
+            this.BlackCat.Name = "BlackCat";
+            this.BlackCat.Size = new System.Drawing.Size(55, 19);
+            this.BlackCat.TabIndex = 1;
+            this.BlackCat.Text = "黒猫";
+            this.BlackCat.UseVisualStyleBackColor = true;
+            this.BlackCat.CheckedChanged += new System.EventHandler(this.ColorButton_CheckedChanged);
+            // 
+            // CatIcon
+            // 
+            this.CatIcon.Text = "ﾆｬｵ";
+            this.CatIcon.Visible = true;
+            this.CatIcon.Click += new System.EventHandler(this.CatIcon_Click);
+            // 
+            // TaskTrayButton
+            // 
+            this.TaskTrayButton.Location = new System.Drawing.Point(12, 66);
+            this.TaskTrayButton.Name = "TaskTrayButton";
+            this.TaskTrayButton.Size = new System.Drawing.Size(75, 23);
+            this.TaskTrayButton.TabIndex = 3;
+            this.TaskTrayButton.Text = "Tasktray";
+            this.TaskTrayButton.UseVisualStyleBackColor = true;
+            this.TaskTrayButton.Click += new System.EventHandler(this.TaskTrayButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(93, 66);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // TasktrayCheck
+            // 
+            this.TasktrayCheck.AutoSize = true;
+            this.TasktrayCheck.Location = new System.Drawing.Point(76, 13);
+            this.TasktrayCheck.Name = "TasktrayCheck";
+            this.TasktrayCheck.Size = new System.Drawing.Size(86, 19);
+            this.TasktrayCheck.TabIndex = 5;
+            this.TasktrayCheck.Text = "自動格納";
+            this.TasktrayCheck.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(183, 101);
+            this.Controls.Add(this.TasktrayCheck);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.TaskTrayButton);
+            this.Controls.Add(this.BlackCat);
+            this.Controls.Add(this.WhiteCat);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "CatRun";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer IntervalTimer;
+        private System.Windows.Forms.Timer FrameTimer;
+        private System.Windows.Forms.RadioButton WhiteCat;
+        private System.Windows.Forms.RadioButton BlackCat;
+        private System.Windows.Forms.NotifyIcon CatIcon;
+        private System.Windows.Forms.Button TaskTrayButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.CheckBox TasktrayCheck;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatRun
+{
+    public partial class MainForm : Form
+    {
+        private Icon[] icons = new Icon[5];
+        private bool interval_flg;
+        private int count_frame = 0;
+        private PerformanceCounter pc;
+        private const int WHITE = 1;
+        private const int BLACK = 2;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (WhiteCat.Checked)
+            { // 白猫
+                Icon[] white ={
+                    Properties.Resources.w1,
+                    Properties.Resources.w2,
+                    Properties.Resources.w3,
+                    Properties.Resources.w4,
+                    Properties.Resources.w5
+                };
+                icons = white;
+            }
+            else if (BlackCat.Checked)
+            { // 黒猫
+                Icon[] black ={
+                    Properties.Resources.b1,
+                    Properties.Resources.b2,
+                    Properties.Resources.b3,
+                    Properties.Resources.b4,
+                    Properties.Resources.b5
+                };
+                icons = black;
+            }
+        }
+
+        private void TaskTrayButton_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+        }
+        private int GetAppsUserThema()
+        { /* ダーク:1 ライト:2 */
+            int getmode = -1; // 操作するレジストリ・キーの名前
+            string rKeyName = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+            // 取得処理を行う対象となるレジストリの値の名前
+            string rGetValueName = "AppsUseLightTheme";
+
+            // レジストリの取得
+            try
+            {
+                // レジストリ・キーのパスを指定してレジストリを開く
+                Microsoft.Win32.RegistryKey rKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(rKeyName);
+
+                // レジストリの値を取得(DWord)
+                getmode = (int)rKey.GetValue(rGetValueName);
+
+                // 開いたレジストリ・キーを閉じる
+                rKey.Close();
+            }
+            catch (NullReferenceException)
+            {
+                // レジストリ・キーまたは値が存在しない
+                Console.WriteLine("レジストリ［" + rKeyName + "］の［" + rGetValueName + "］がありません！");
+            }
+            return getmode + 1;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.tasktray)
+                TasktrayCheck.Checked = true;
+            else
+                TasktrayCheck.Checked = false;
+
+            if (Properties.Settings.Default.color == 0)
+            {
+                int userThema = GetAppsUserThema();
+                if (userThema == 1) WhiteCat.Checked = true;
+                else if (userThema == 2) BlackCat.Checked = true;
+            }
+            else if (Properties.Settings.Default.color == WHITE)
+                WhiteCat.Checked = true;
+            else if (Properties.Settings.Default.color == BLACK)
+                BlackCat.Checked = true;
+            else
+                BlackCat.Checked = true;
+
+            pc = new PerformanceCounter("Processor", "% Processor Time", "_Total", true);
+            CatIcon.Visible = true;
+            IntervalTimer.Start();
+            FrameTimer.Start();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            if (TasktrayCheck.Checked)
+                Visible = false;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (TasktrayCheck.Checked) Properties.Settings.Default.tasktray = true;
+            else Properties.Settings.Default.tasktray = false;
+            if (WhiteCat.Checked) Properties.Settings.Default.color = WHITE;
+            else if (BlackCat.Checked) Properties.Settings.Default.color = BLACK;
+            Properties.Settings.Default.Save();
+        }
+
+        private void IntervalTimer_Tick(object sender, EventArgs e)
+        {
+            interval_flg = true;
+        }
+
+        private void FrameTimer_Tick(object sender, EventArgs e)
+        {
+            if (interval_flg)
+            {
+                // CPU稼働率処理
+                this.FrameTimer.Interval = 150 - (int)pc.NextValue(); // CPU使用率に応じて
+                interval_flg = false;
+            }
+            count_frame = (count_frame + 1) % icons.Length;
+            CatIcon.Icon = icons[count_frame];
+        }
+
+        private void CatIcon_Click(object sender, EventArgs e)
+        {
+            Visible = true;
+        }
+    }
+}
